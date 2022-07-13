fc35daf947864cec61062b065cb4c26f877acc47
realMNohgee <clund25@gmail.com>
2022-07-13 13:02:02 -0500

    set a new def and added some notes for what I am going to do next

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -34,22 +34,28 @@ end
 
 puts 'EventManager initialized.'
 
-contents = CSV.open(
-  'event_attendees.csv',
-  headers: true,
-  header_converters: :symbol
-)
+  contents = CSV.open(
+    'event_attendees.csv',
+    headers: true,
+    header_converters: :symbol
+  )
 
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
-contents.each do |row|
-  id = row[0]
-  name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
-  legislators = legislators_by_zipcode(zipcode)
 
-  form_letter = erb_template.result(binding)
+# encapsulates code for clean up zipcodes, pull officials and create thank you letters #
+def exercise_output(contents)
+  contents.each do |row|
+    id = row[0]
+    name = row[:first_name]
+    zipcode = clean_zipcode(row[:zipcode])
+    legislators = legislators_by_zipcode(zipcode)
 
-  save_thank_you_letter(id,form_letter)
-end
+    form_letter = erb_template.result(binding)
+
+    save_thank_you_letter(id,form_letter)
+  end
+
+  # something to pull the phone numbers from input file and normalize removing unwanted charaters #
+  # also runs normalized numbers through check method and outs first name with number #
