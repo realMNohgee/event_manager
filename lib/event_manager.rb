b4a32fb6df3e562dfea70e1392cea1ddc2f19ef9
realMNohgee <clund25@gmail.com>
2022-07-13 14:10:29 -0500

    switching it up.  I'm starting over with if/elsif/else for cleaning up the numbers so they are valid when entered.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -12,17 +12,17 @@ def legislators_by_zipcode(zip)
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    civic_info.representative_info_by_address(
+    legislators = civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
     ).officials
-  rescue
+  rescue => exception
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
 
-def save_thank_you_letter(id,form_letter)
+def save_thank_you_letter(id, form_letter)
   Dir.mkdir('output') unless Dir.exist?('output')
 
   filename = "output/thanks_#{id}.html"
@@ -32,83 +32,15 @@ def save_thank_you_letter(id,form_letter)
   end
 end
 
-puts 'EventManager initialized.'
-
-  contents = CSV.open(
-    'event_attendees.csv',
-    headers: true,
-    header_converters: :symbol
-  )
-
-template_letter = File.read('form_letter.erb')
-erb_template = ERB.new template_letter
-
-
-# encapsulates code for clean up zipcodes, pull officials and create thank you letters #
-def exercise_output(contents)
-  contents.each do |row|
-    id = row[0]
-    name = row[:first_name]
-    zipcode = clean_zipcode(row[:zipcode])
-    legislators = legislators_by_zipcode(zipcode)
-
-    form_letter = erb_template.result(binding)
-
-    save_thank_you_letter(id,form_letter)
+def clean_numbers(phone_number)
+  phone_number = phone_number.tr('^0-9', '')
+  if phone_number.length == 10
+    phone_number
+  elsif phone_number.length == 11 && phone_number[0] == '1'
+    phone_number[1..-1]
+  else
+    'The phone number you entered was not valid!'
   end
 end
 
-
-  # something to pull the phone numbers from input file and normalize removing unwanted charaters #
-  # also runs normalized numbers through check method and outs first name with number #
-  # validate and correct numbers #
-  def phone_numbers(contents)
-    contents.each do |row|
-      phone_number = row[:home_phone].tr('()-." "', '')
-      puts "#{check_phone_number(phone_number)}: #{row[:first_name]}"
-    end
-  end
-
-  def check_phone_number(phone_number)
-    case 
-    when phone_number.length < 10 || phone_number.length > 11
-      print 'invaild number'
-    when phone_number.length == 11 
-      phone_number.start_with?('1') ? (print phone_number[1..-1]) : (print 'invalid number')
-    else
-      print phone_number
-    end
-  end
-
-def registration_time(contents)
-  time_array = []
-  contents.each do |row|
-    date_time = DateTime.strptime(row[:regdate], '%m/%d/%y %k')
-    time_array << date_time.hour
-  end
-
-    hour_dist = Hash.new(0)
-    time_array.each |hour|
-      hour_dist[hour] += 1
-  end
-  puts hour_dist
-end
-
-def registration_day(contents)
-  day_array = []
-  contents.each do |row|
-    date_time = DateTime.strptime(row[:regdate], '%m/%d/%y')
-    day_array << date_time.strftime('%A')
-  end
-
-  day_dist = Hash.new(0)
-  day_array.each do |day|
-    day_dist[day] += 1
-  end
-  puts day_dist
-end
-
-puts 'The phone numbers are as follows:'
-phone_numbers(contents)
-
-
+def get_reg_hour(reg_date)
